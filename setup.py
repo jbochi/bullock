33f452b656c9a492d425cefce6cfef360b6a915d
Juarez Bochi <jbochi@gmail.com>
2014-01-08 14:25:58 -0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from os.path import dirname, abspath, join
+from setuptools import setup
+
+with open(abspath(join(dirname(__file__), 'README.rst'))) as fileobj:
+    README = fileobj.read().strip()
+
+install_reqs = [req for req in open(abspath(join(dirname(__file__), 'requirements.txt')))]
+
+setup(
+    name='bullock',
+    description='Distributed lock using Redis',
+    long_description=README,
+    author='Juarez Bochi',
+    version='0.0.1',
+    include_package_data=True,
+    install_requires=install_reqs,
+    packages=['bullock'],
+)
