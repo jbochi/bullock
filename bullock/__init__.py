f1fa81909bea7c227ddbd2acbc38f77fd78814a1
Juarez Bochi <jbochi@gmail.com>
2014-01-08 10:44:34 -0200

    Initial commit

---
+++ bullock/__init__.py
@@ -0,0 +1,10 @@
+import redis
+import uuid
+
+class Bullock(object):
+    def __init__(self, key=None, host='localhost', port=6379, db=0):
+        self.key = key if key else uuid.uuid4()
+        self.redis = redis.StrictRedis(host=host, port=port, db=db)
+
+    def lock(self):
+        return self.redis.setnx(self.key, "locked")
---
+++ tests/test_bullock.py
@@ -0,0 +1,27 @@
+import redis
+import bullock
+
+TEST_DB = 14
+
+class Bullock(bullock.Bullock):
+    "A modified lock that uses a db different from default since we flush it before all tests"
+    def __init__(self, *args, **kwargs):
+        kwargs['db'] = TEST_DB
+        super(Bullock, self).__init__(*args, **kwargs)
+
+
+def setup_function(func):
+    r = redis.StrictRedis(db=TEST_DB)
+    r.flushdb()
+
+def test_can_acquire_lock():
+    b = Bullock()
+    assert b.lock()
+
+
+def test_cannot_acquire_lock_if_other_is_locking():
+    b1 = Bullock(key="mykey")
+    assert b1.lock()
+
+    b2 = Bullock(key="mykey")
+    assert not b2.lock()
